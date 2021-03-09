182b933d677567730f53fbcb5e70df71a243fb51
Alexey <winderton228@yandex.ru>
2021-03-09 15:49:23 +0500

    Добавьте файлы проекта.

---
+++ BigInt.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LaboratoryWork1
+{
+    public class BigInt
+    {
+        private readonly List<byte> number = new List<byte>();
+        public bool IsPositiveSign { get; }
+        public int Count => number.Count;
+        public byte this[int index] => number[index];
+
+        public string Value
+        {
+            get
+            {
+                var result = new StringBuilder();
+                if (!IsPositiveSign)
+                    result.Append("-");
+                for (var i = Count - 1; i >= 0; --i)
+                    result.Append(number[i]);
+                return result.ToString();
+            }
+        }
+
+        public BigInt()
+        {
+            IsPositiveSign = true;
+            number.Add(0);
+        }
+
+        public BigInt(string value)
+        {
+            IsPositiveSign = value[0] != '-';
+            if (!IsPositiveSign)
+                value = value.Remove(0, 1);
+            for (var i = value.Length - 1; i >= 0; --i)
+                number.Add(byte.Parse(value[i].ToString()));
+        }
+
+        private BigInt(bool isPositiveSign, List<byte> number)
+        {
+            IsPositiveSign = isPositiveSign;
+            this.number = number;
+        }
+
+        public static BigInt operator+(BigInt value1, BigInt value2)
+        {
+            var result = new List<byte>();
+            var rank = 0;
+            var index = 0;
+            for (; index < value1.Count && index < value2.Count; ++index)
+            {
+                var value = value1[index] + value2[index] + rank;
+                result.Add((byte)(value % 10));
+                rank = value / 10;
+            }
+
+            var remainder = index < value1.Count
+                ? value1
+                : value2;
+
+            for (; index < remainder.Count; ++index)
+            {
+                var value = remainder[index] + rank;
+                result.Add((byte)(value % 10));
+                rank = value / 10;
+            }
+            if (rank > 0)
+                result.Add((byte)rank);
+
+            return new BigInt(value1.IsPositiveSign, result);
+        }
+
+        public static BigInt operator -(BigInt value1, BigInt value2)
+        {
+            var resultSign = value1 > value2;
+            var newValue1 = value1 > value2 ? value1 : value2;
+            var newValue2 = value1 > value2 ? value2 : value1;
+
+            var result = new List<byte>();
+            var rank = 0;
+            var index = 0;
+            for (; index < newValue1.Count && index < newValue2.Count; ++index)
+            {
+                var value = newValue1[index] - newValue2[index] - rank;
+                if (value < 0)
+                {
+                    value += 10;
+                    rank = value / 10 + 1;
+                }
+                else rank = value / 10;
+
+                result.Add((byte) (value % 10));
+            }
+
+            var remainder = index < newValue1.Count
+                ? newValue1
+                : newValue2;
+
+            for (; index < remainder.Count; ++index)
+            {
+                var value = remainder[index] - rank;
+                if (value < 0)
+                {
+                    value += 10;
+                    rank = value / 10 + 1;
+                }
+                else rank = value / 10;
+
+                result.Add((byte)(value % 10));
+            }
+
+            if (rank > 0)
+                result.Add((byte) rank);
+            while (result[result.Count - 1] == 0)
+                result.RemoveAt(result.Count - 1);
+
+            return new BigInt(resultSign, result);
+        }
+
+        public static bool operator ==(BigInt value1, BigInt value2)
+        {
+            if (value1.IsPositiveSign != value2.IsPositiveSign || value1.Count != value2.Count)
+                return false;
+            for (var i = 0; i < value1.Count; ++i)
+                if (value1[i] != value2[i])
+                    return false;
+            return true;
+        }
+
+        public static bool operator !=(BigInt value1, BigInt value2) => !(value1 == value2);
+
+        public static bool operator >(BigInt value1, BigInt value2)
+            => value1.IsPositiveSign != value2.IsPositiveSign
+                ? value1.IsPositiveSign
+                : CompareWithPredicate(value1, value2, (x, y) => x > y);
+
+        public static bool operator >=(BigInt value1, BigInt value2)
+            => value1 > value2 || value1 == value2;
+
+        public static bool operator <(BigInt value1, BigInt value2)
+            => value1.IsPositiveSign != value2.IsPositiveSign 
+                ? value2.IsPositiveSign 
+                : CompareWithPredicate(value1, value2, (x, y) => x < y);
+
+        public static bool operator <=(BigInt value1, BigInt value2)
+            => value1 < value2 || value1 == value2;
+
+        private static bool CompareWithPredicate(BigInt value1, BigInt value2, Func<int, int, bool> predicate)
+        {
+            if (value1.Count != value2.Count)
+                return value1.IsPositiveSign
+                    ? predicate(value1.Count, value2.Count)
+                    : !predicate(value1.Count, value2.Count);
+
+            for (var i = value1.Count - 1; i >= 0; --i)
+                if (value1[i] != value2[i])
+                    return value1.IsPositiveSign
+                        ? predicate(value1[i], value2[i])
+                        : !predicate(value1[i], value2[i]);
+            return false;
+        }
+    }
+}
---
+++ BigInt_Tests.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace LaboratoryWork1
+{
+    internal class BigIntTests
+    {
+        [TestCase("256", "256", true, TestName = "WhenSamePositiveNumbers")]
+        [TestCase("256", "26", false, TestName = "WhenPositiveNumbers")]
+        [TestCase("-256", "-256", true, TestName = "WhenSameNegativeNumbers")]
+        [TestCase("-256", "-26", false, TestName = "WhenNegativeNumbers")]
+        [TestCase("256", "-26", false, TestName = "WhenPositiveAndNegativeNumbers")]
+        public void CorrectEqual(string value1, string value2, bool isEqual)
+        {
+            Assert.AreEqual(isEqual, new BigInt(value1) == new BigInt(value2));
+        }
+
+        [TestCase("256", "256", false, TestName = "WhenSamePositiveNumbers")]
+        [TestCase("256", "26", true, TestName = "WhenPositiveNumbers")]
+        [TestCase("-256", "-256", false, TestName = "WhenSameNegativeNumbers")]
+        [TestCase("-26", "-256", true, TestName = "WhenNegativeNumbers")]
+        [TestCase("26", "-256", true, TestName = "WhenPositiveAndNegativeNumbers")]
+        public void CorrectCompare_GreaterThan(string value1, string value2, bool isEqual)
+        {
+            Assert.AreEqual(isEqual, new BigInt(value1) > new BigInt(value2));
+        }
+
+        [TestCase("264", "264", false, TestName = "WhenSamePositiveNumbers")]
+        [TestCase("25", "26", true, TestName = "WhenPositiveNumbers")]
+        [TestCase("-256", "-256", false, TestName = "WhenSameNegativeNumbers")]
+        [TestCase("-257", "-256", true, TestName = "WhenNegativeNumbers")]
+        [TestCase("-26", "26", true, TestName = "WhenPositiveAndNegativeNumbers")]
+        public void CorrectCompare_LessThan(string value1, string value2, bool isEqual)
+        {
+            var first = new BigInt(value1);
+            Assert.AreEqual(isEqual, first < new BigInt(value2));
+        }
+
+        [TestCase("847", "178", "1025", TestName = "WhenNumbersHaveSameLength")]
+        [TestCase("95841", "177478", "273319", TestName = "WhenLeftNumberIsShorter")]
+        [TestCase("177478", "95841", "273319", TestName = "WhenRightNumberIsShorter")]
+        [TestCase("17747984891984498489498489", "494981874984984894984984", "18242966766969483384483473", TestName = "WhenBigNumbers")]
+        public void CorrectAddition_WhenBothNumberArePositive(string value1, string value2, string expectedValue)
+        {
+            var number1 = new BigInt(value1);
+            var number2 = new BigInt(value2);
+
+            var actual = number1 + number2;
+
+            Assert.AreEqual(new BigInt(expectedValue).Value, actual.Value);
+        }
+
+        [TestCase("-847", "-178", "-1025", TestName = "WhenNumbersHaveSameLength")]
+        [TestCase("-95841", "-177478", "-273319", TestName = "WhenLeftNumberIsShorter")]
+        [TestCase("-177478", "-95841", "-273319", TestName = "WhenRightNumberIsShorter")]
+        [TestCase("-17747984891984498489498489", "-494981874984984894984984", "-18242966766969483384483473", TestName = "WhenBigNumbers")]
+        public void CorrectAddition_WhenBothNumberAreNegative(string value1, string value2, string expectedValue)
+        {
+            var number1 = new BigInt(value1);
+            var number2 = new BigInt(value2);
+
+            var actual = number1 + number2;
+
+            Assert.AreEqual(new BigInt(expectedValue).Value, actual.Value);
+        }
+
+        [TestCase("847", "178", "669", TestName = "WhenNumbersHaveSameLength")]
+        [TestCase("95841", "177478", "-81637", TestName = "WhenLeftNumberIsShorter")]
+        [TestCase("177478", "95841", "81637", TestName = "WhenRightNumberIsShorter")]
+        [TestCase("17747984891984498489498489", "41949818749849848949840984", "-24201833857865350460342495",
+            TestName = "WhenBigNumbers")]
+        public void CorrectSubtraction_WhenBothNumberArePositive(string value1, string value2, string expectedValue)
+        {
+            var number1 = new BigInt(value1);
+            var number2 = new BigInt(value2);
+
+            var actual = number1 - number2;
+
+            Assert.AreEqual(new BigInt(expectedValue).Value, actual.Value);
+        }
+    }
+}
